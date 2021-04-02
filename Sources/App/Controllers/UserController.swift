ce16aecfc4181db60648afa3505964bc0255e91d
Mikhail <wowivan2011@yandex.ru>
2021-04-02 16:32:28 +0300

    added push on topic. Refactoring code

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -1,111 +1,20 @@
 //
-//  Device.swift
+//  UserController.swift
 //  
 //
-//  Created by Mikhail Ivanov on 23.03.2021.
+//  Created by Mikhail Ivanov on 02.04.2021.
 //
 
 import Vapor
 import Fluent
-import FCM
-import APNS
-import JWT
 
-final class DeviceController {
+final class UserController {
     init(_ app: Application) {
-        app.post("test-push", use: testPush)
+        app.post(["user", "addDevice"], use: registrateDevice)
         
-        app.post("push", use: push)
-        
-        app.post("addDevice", use: registrateDevice)
-        
-        app.get("users", use: getUsers)
+        app.get(["user", "all"], use: getUsers)
         
         app.post("user", use: getUser)
-        
-        // TODO: test
-        if app.environment == .development {
-            app.delete("drop", use: dropDB)
-        }
-    }
-    
-    func testPush(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        let push = try req.content.decode(PushMessage.self)
-        
-        guard let config = ConfigurationService.loadSettings(),
-              let deviceID = config.testDevice else {
-            return req.eventLoop.makeFailedFuture(Abort(.custom(code: 404, reasonPhrase: "Not found deviceID")))
-        }
-        
-        return req.apns.send(
-            .init(title: push.title, subtitle: push.message),
-            to: deviceID
-        ).map { .ok }
-        
-    }
-    
-    func push(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        
-        ///     TODO: remove comment before deployment
-        //      let _ = try req.jwt.verify(as: ITLabPayload.self)
-        
-        let push = try req.content.decode(PushClient.self)
-        
-        return DeviceInfo.query(on: req.db).filter(\.$user.$id == push.userId).all().flatMap { (devices) -> EventLoopFuture<HTTPStatus> in
-            if devices.isEmpty {
-                return req.eventLoop.makeFailedFuture(Abort(.notFound))
-            }
-            
-            var task: [EventLoopFuture<Void>] = []
-            
-            devices.forEach { device in
-                switch device.type {
-                case .ios:
-                    task.append(
-                        req.apns.send(
-                            .init(title: push.push.title, subtitle: nil, body: push.push.message),
-                            to: device.deviceID
-                        ).flatMapError{ (e) -> EventLoopFuture<Void> in
-                            if let error = e as? APNSwiftError.ResponseError {
-                                switch error {
-                                case .badRequest(.badDeviceToken):
-                                    req.logger.info("Device \(String(describing: device.id!)) of type iOS has been removed from the database")
-                                    return device.delete(on: req.db)
-                                default:
-                                    break
-                                }
-                            }
-                            
-                            return req.eventLoop.makeSucceededVoidFuture()
-                        })
-                    break
-                    
-                case .android:
-                    let notification = FCMNotification(title: push.push.title, body: push.push.message)
-                    let message = FCMMessage(token: device.deviceID, notification: notification)
-                    
-                    task.append(req.fcm.send(message).flatMapAlways{ (result) in
-                        switch result {
-                        case .failure(let e):
-                            if let error = e as? GoogleError,
-                               error.code == 404 || error.code == 410 {
-                                req.logger.info("Device \(String(describing: device.id!)) of type Android has been removed from the database")
-                                return device.delete(on: req.db)
-                            }
-                            break
-                        case .success(_):
-                            break
-                        }
-                        return req.eventLoop.makeSucceededVoidFuture()
-                    })
-                    break
-                }
-                
-            }
-            
-            return task.flatten(on: req.eventLoop).transform(to: HTTPStatus.created)
-            
-        }
     }
     
     func registrateDevice(_ req: Request) throws -> EventLoopFuture<UserDevicesServer> {
@@ -153,9 +62,9 @@ final class DeviceController {
             let userDB = UserDevices(id: user.id)
             return userDB.create(on: req.db).flatMap { () -> EventLoopFuture<UserDevicesServer> in
                 
-                let device = DeviceInfo(type: user.device.type, deviceID: user.device.deviceID, user: userDB)
+                let device = DeviceInfo(type: user.device.type, deviceID: user.device.deviceID)
                 
-                return device.create(on: req.db).map { () -> UserDevicesServer in
+                return userDB.$devices.create(device, on: req.db).map { () -> UserDevicesServer in
                     
                     let deviceResult = DeviceServer(id: device.id!, deviceID: device.deviceID, type: device.type)
                     
@@ -164,12 +73,6 @@ final class DeviceController {
                     return result
                 }
             }
-        }
-    }
-    
-    func dropDB(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        return DeviceInfo.query(on: req.db).delete().flatMap {() -> EventLoopFuture<HTTPStatus> in
-            return UserDevices.query(on: req.db).delete().map {.ok}
         }
     }
     
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
     
-    let _ = DeviceController(app)
+    let _ = UserController(app)
     
     let _ = TopicController(app)
+    
+    let _ = PushController(app)
+
 }
