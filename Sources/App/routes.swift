f965cd6cf8d4e235fcf276d00b23415051c93507
Mikhail <wowivan2011@yandex.ru>
2021-05-13 11:07:22 +0300

    Added root req

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
 
+    app.get { req -> String in
+        return "I'm push service"
+    }
+    
     let socket = WebSocketController(app)
 
     _ = UserController(app)
